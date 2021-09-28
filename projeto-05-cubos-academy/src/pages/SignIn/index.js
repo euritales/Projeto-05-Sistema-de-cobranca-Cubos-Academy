06a6d0063e294bd0819244599313b8f288a72f35
euritales <58851676+euritales@users.noreply.github.com>
2021-09-28 12:10:18 -0300

    componentização de inputs para utilizar no signin e signup

--- projeto-05-cubos-academy/src/pages/SignIn/index.js
+++ projeto-05-cubos-academy/src/pages/SignIn/index.js
@@ -2,32 +2,14 @@ import "./styles.css";
 import "../../styles/form.css";
 import { Link } from "react-router-dom";
 import logoCubos from "../../assets/logoCubos.svg";
+import InputForm from "../../components/InputForm";
 
 function SignIn() {
   return (
     <div className="container-form flex-column">
       <form className="form form-sign-in">
         <img src={logoCubos} alt="CubosAcademy" />
-        <div>
-          <div className="flex-column input">
-            <label htmlFor="email">E-mail</label>
-            <input
-              type="text"
-              name="email"
-              id="email"
-              placeholder="exemplo@gmail.com"
-            />
-          </div>
-          <div className="flex-column input">
-            <label htmlFor="password">Senha</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="minhasenha"
-            />
-          </div>
-        </div>
+        <InputForm />
         <button className="btn btn-opaque">Entrar</button>
       </form>
       <div>
