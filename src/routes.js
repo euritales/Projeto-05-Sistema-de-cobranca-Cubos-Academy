36667be3a1bf5561c05908d461b6159ad65b7882
euritales <58851676+euritales@users.noreply.github.com>
2021-10-01 20:15:00 -0300

    ae, finalmente

--- src/routes.js
+++ src/routes.js
@@ -10,8 +10,8 @@ function Routes() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={Main} />
-        <Route path="/sign-in" exact component={SignIn} />
+        <Route path="/" exact component={SignIn} />
+        <Route path="/home" exact component={Main} />
         <Route path="/sign-up" exact component={SignUp} />
         <Route path="/charge" exact component={Charges} />
         <Route path="/profile" exact component={Profile} />
