7a86495f4d34b44569bc0a227e6ad4163c404630
euritales <58851676+euritales@users.noreply.github.com>
2021-10-04 16:01:50 -0300

    savePoint

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,14 +1,16 @@
 import "./styles.css";
 import SadIcon from "../../assets/sad-icon.svg";
 import HappyIcon from "../../assets/happy-icon.svg";
+import EditProfile from "../../components/EditProfile";
 
 function Profile() {
   return (
-    <div className="container-profile">
-      <img src={SadIcon} alt="" />
-      <span>Site ainda em construção. Volte em breve!</span>
-      <img src={HappyIcon} alt="" />
-    </div>
+    // <div className="container-profile">
+    //   <img src={SadIcon} alt="" />
+    //   <span>Site ainda em construção. Volte em breve!</span>
+    //   <img src={HappyIcon} alt="" />
+    // </div>
+    <EditProfile />
   );
 }
 
