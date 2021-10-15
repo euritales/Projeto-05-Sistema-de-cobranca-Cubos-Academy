dce4821afbfdd1b9814331a9f2c8c1539b5e3e3f
euritales <58851676+euritales@users.noreply.github.com>
2021-10-15 02:15:12 -0300

    implementacao nova mascara

--- src/pages/Customers/index.js
+++ src/pages/Customers/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import { useHistory } from "react-router-dom";
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { AuthContext } from "../../context/auth";
 import { ClientContext } from "../../context/client";
 import EmailIcon from "../../assets/email-icon.svg";
@@ -8,11 +8,16 @@ import PhoneIcon from "../../assets/phone-icon.svg";
 import EditIcon from "../../assets/edit-icon.svg";
 import NumberFormat from "react-number-format";
 import SearchIcon from "../../assets/search-icon.svg";
+import EditCustomers from "../../components/EditCustomers";
+import DetailsCustomers from "../../components/DetailsCustomers";
+import { formatToBRL } from "brazilian-values";
 
 function Customers() {
   const history = useHistory();
   const { token } = useContext(AuthContext);
   const { getClients, clients } = useContext(ClientContext);
+  const [editClients, setEditClients] = useState(false);
+  const [detailsClient, setDetailsClient] = useState(false);
 
   useEffect(() => {
     async function callGetClient() {
@@ -21,114 +26,107 @@ function Customers() {
     callGetClient();
   }, []);
 
+  {
+    /* {detailsClient && (
+    <DetailsCustomers setDetailsClient={setDetailsClient} />
+  )} */
+  }
   return (
     <div className="container-costumers">
-      <div className="input-busca-botao">
-        <button
-          className="btn btn-white"
-          onClick={() => history.push("/clients/register")}
-        >
-          Adicionar cliente
-        </button>
-        <div>
-          <input placeholder="Procurar por Nome, E-mail ou CPF" />
-          <button>
-            <img src={SearchIcon} alt="" />
-          </button>
-        </div>
-      </div>
-      <div className="container-description-costumers">
-        <span className="span-lg">Cliente</span>
-        <span className="span-lg">Cobranças Feitas</span>
-        <span className="span-lg">Cobranças Recebidas</span>
-        <span>Status</span>
-      </div>
-      <div className="box-container-details">
-        {!clients ? (
-          <div className="no-register">
-            <h3>Sem registros no momento!</h3>
+      {editClients ? (
+        <EditCustomers setEditClients={setEditClients} />
+      ) : (
+        <>
+          <div className="input-busca-botao">
+            <button
+              className="btn btn-white"
+              onClick={() => history.push("/clients/register")}
+            >
+              Adicionar cliente
+            </button>
+            <div>
+              <input placeholder="Procurar por Nome, E-mail ou CPF" />
+              <button>
+                <img src={SearchIcon} alt="" />
+              </button>
+            </div>
           </div>
-        ) : (
-          clients.map(
-            ({
-              id,
-              nome,
-              cpf,
-              email,
-              cep,
-              telefone,
-              so_pago,
-              valor_cobrado,
-              statusCliente,
-            }) => (
-              <div className="container-details-costumers" key={id}>
-                <div className="client-details">
-                  <div
-                    onClick={() =>
-                      history.push(`/clients/${id}/details/`, {
-                        id,
-                      })
-                    }
-                    className="name-costumers"
-                  >
-                    <button>
-                      <p>{nome}</p>
-                    </button>
-                  </div>
-                  <div className="span-client-email">
-                    <img src={EmailIcon} alt="email" />
-                    <span>{email}</span>
-                  </div>
-                  <div>
-                    <img src={PhoneIcon} alt="telefone" />
-                    <NumberFormat
-                      value={telefone}
-                      displayType={"text"}
-                      format="(##) # ####-####"
-                    />
-                  </div>
-                </div>
-                <NumberFormat
-                  className="span-lg "
-                  value={!valor_cobrado ? "0" : valor_cobrado}
-                  displayType={"text"}
-                  thousandSeparator={true}
-                  prefix={"R$"}
-                />
-                <NumberFormat
-                  className="span-lg margin-lg"
-                  value={so_pago}
-                  displayType={"text"}
-                  thousandSeparator={true}
-                  prefix={"R$"}
-                />
-
-                <span
-                  className={`status-costumers ${statusCliente.toLowerCase()}`}
-                >
-                  {statusCliente.toUpperCase()}
-                </span>
-                <div className="edit-clients-button">
-                  <button
-                    onClick={() =>
-                      history.push(`/clients/${id}/edit`, {
-                        id,
-                        nome,
-                        cpf,
-                        cep,
-                        email,
-                        telefone,
-                      })
-                    }
-                  >
-                    <img src={EditIcon} alt="Editar Cliente" />
-                  </button>
-                </div>
+          <div className="container-description-costumers">
+            <span className="span-lg">Cliente</span>
+            <span className="span-lg">Cobranças Feitas</span>
+            <span className="span-lg">Cobranças Recebidas</span>
+            <span>Status</span>
+          </div>
+          <div className="box-container-details">
+            {!clients ? (
+              <div className="no-register">
+                <h3>Sem registros no momento!</h3>
               </div>
-            )
-          )
-        )}
-      </div>
+            ) : (
+              clients.map(
+                ({
+                  id,
+                  nome,
+                  cpf,
+                  email,
+                  cep,
+                  telefone,
+                  so_pago,
+                  valor_cobrado,
+                  statusCliente,
+                }) => (
+                  <div className="container-details-costumers" key={id}>
+                    <div className="client-details">
+                      <div
+                        onClick={() =>
+                          history.push(`/clients/${id}/details/`, {
+                            id,
+                          })
+                        }
+                        className="name-costumers"
+                      >
+                        <button>
+                          <p>{nome}</p>
+                        </button>
+                      </div>
+                      <div className="span-client-email">
+                        <img src={EmailIcon} alt="email" />
+                        <span>{email}</span>
+                      </div>
+                      <div>
+                        <img src={PhoneIcon} alt="telefone" />
+                        <NumberFormat
+                          value={telefone}
+                          displayType={"text"}
+                          format="(##) # ####-####"
+                        />
+                      </div>
+                    </div>
+
+                    <span className="span-lg  ">
+                      {formatToBRL(!valor_cobrado ? "0" : valor_cobrado / 100)}
+                    </span>
+                    <span className="span-lg margin-lg">
+                      {formatToBRL(so_pago / 100)}
+                    </span>
+
+                    <span
+                      className={`status-costumers ${statusCliente.toLowerCase()}`}
+                    >
+                      {statusCliente.toUpperCase()}
+                    </span>
+                    <div className="edit-clients-button">
+                      <button onClick={() => setEditClients(true)}>
+                        <img src={EditIcon} alt="Editar Cliente" />
+                      </button>
+                    </div>
+                  </div>
+                )
+              )
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 }
