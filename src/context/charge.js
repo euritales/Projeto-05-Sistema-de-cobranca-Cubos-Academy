6b7bb5481a0649fdbceca168da8935f2790cb5ab
euritales <58851676+euritales@users.noreply.github.com>
2021-10-19 19:25:56 -0300

    reparo

--- src/context/charge.js
+++ src/context/charge.js
@@ -238,6 +238,7 @@ export const ChargeContextProvider = ({ children }) => {
       return ErrorMessage(error.message);
     }
   }
+
   async function deleteCharge(id, setOpenEditCharges) {
     try {
       const response = await fetch(
